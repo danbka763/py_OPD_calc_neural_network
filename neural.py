cd4fd549afc20d5a512475e173dcf529511b52db
Having-no-guise <eskelanir@mail.ru>
2022-03-17 16:33:49 +0700

    master

--- neural.py
+++ neural.py
@@ -1,14 +1,14 @@
 from tensorflow import keras
 from keras import layers
 import numpy as np
-
+from keras.models import load_model
 from modules import generationData, vectorizationData, buildModel, educateNeural
 
 
 chars = "0123456789+ "
 
 # Параметры для модели и набора данных.
-TRAINING_SIZE = 50000
+'''TRAINING_SIZE = 50000
 DIGITS = 3
 REVERSE = True
 
@@ -18,7 +18,7 @@ MAXLEN = DIGITS + 1 + DIGITS
 
 questions, expected, ctable = generationData.gen(np, chars, TRAINING_SIZE, DIGITS, REVERSE, MAXLEN)
 
-print(questions)
+#print(questions)
 
 x_train, y_train, x_val, y_val = vectorizationData.vect(np, questions, expected, ctable, chars, DIGITS, MAXLEN)
 
@@ -29,8 +29,16 @@ model = buildModel.build(keras, layers, DIGITS, MAXLEN, chars)
 print()
 
 model = educateNeural.educate(np, x_train, y_train, x_val, y_val, model, REVERSE, ctable)
+model.save('my_model.h5')'''
+model = load_model('my_model.h5')
 
 
 # Погружаем данные в виде '312+214', ' 12+214', '  12+21', ...
-def calc(str):
-    pass
+# Пока не понятен тип данных на вход
+def calc(input_data):
+    question = np.array(input_data.split('+'), dtype='float32')
+    answ = model(question)  # вызывает ошибку - неверный ввод
+    return answ  # передать на выход
+
+
+print(calc('3+2'))  #тестовый вызов функции
